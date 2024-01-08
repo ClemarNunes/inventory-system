8448adfd79081b0bd6c2793964852f5d2e8af676
delivery-store <clemar_nunes@hotmail.com>
2024-01-08 23:52:05 +0000

    structured input and output layout

--- pages/InputOutput/index.tsx
+++ pages/InputOutput/index.tsx
@@ -1,8 +1,22 @@
+import styles from './InputOutput.module.css';
 
 const InputOutput = () => {
-    return(
-        <div>
-            pagina InputOutput
+    return (
+        <div className={styles.Container}>
+            <div className={styles.painel}>
+                <div className={styles.MenuArea}>
+
+                    <div className={styles.menuItem}>
+                        ...
+                    </div>
+                    <div className={styles.menuItem}>
+                        ...
+                    </div>
+                    <div className={styles.menuItem}>
+                        ...
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
